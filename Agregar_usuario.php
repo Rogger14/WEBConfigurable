2c0a3dea782ba3355747251b5301afcc53b2cc3b
Fáride Magnolia <farimag16@gmail.com>
2018-10-04 20:27:45 -0500

    Ventanas configurables
    Agregar un nuevo usuario, lista de las imagenes y la ventana emergente del usuario de servicio.

---
+++ Agregar_usuario.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Agregar usuario</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	<style>
+		header{
+			font-family: Notable;
+			color: #fff;
+			background-color: #3e3f42;
+			padding-top: 20px;
+			padding-left: 20px;
+			padding-bottom: 20px;
+			border-bottom: 4px #fff solid;
+		}
+
+		header .logo{
+			min-height: 62px;
+			max-height: 62px;
+			min-width: 155px;
+		}
+
+		.menu{
+			background-color: #f2f2f2;
+		}
+
+		.opcion_menu{
+			margin-right: 20px;
+		}
+
+		.items{
+			color: gray;
+			font-family: 'Georgia';
+		}
+
+		.contenido {
+			margin-top: 30px;
+		}
+	</style>
+</head>
+<body>
+<div class="container-fluid">
+		<header class="row">
+			<div class="col-3 col-xs-12">
+				<img  src="icono_config/logo.png" alt="Logo Empresa" class="img-fluid logo">
+			</div>
+			<div class="col-1 offset-4">
+				<img  src="icono_config/user.jpg" alt="Foto" class="rounded-circle foto" width="65" height="65">
+			</div>
+			<div class="col-4">
+				<p>Usuario Administrador</p>
+				<p>Magallanes Magallanes, Fáride</p>
+			<div class="col-1 offset-7">
+	 	<button class="btn btn-link" style= "position: absolute;bottom: 10px">
+		<img src="icono_config/icono_salir.png"></button>
+			</div>
+		</header>
+		<section class="row menu">
+			<div class="col-12">
+				<div class="btn-group" role="group" aria-label="Basic example">
+				
+				<div class="dropdown opcion_menu">
+				  <button class="btn btn-link items dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				    Acceso
+				  </button>
+				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				    <a class="dropdown-item" href="Cabecera.html">Menu 1</a>
+				    <a class="dropdown-item" href="#">Menu 2</a>
+				    <a class="dropdown-item" href="#">Menu 3</a>
+				  </div>
+				</div>
+		<div class="dropdown opcion_menu">
+				  <button class="btn btn-link items dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				    Portada Principal
+				  </button>
+				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				    <a class="dropdown-item" href="#">Cabecera de Portada</a>
+				    <a class="dropdown-item" href="#">Redes Sociales</a>
+				    <a class="dropdown-item" href="#">Slider</a>
+				    <a class="dropdown-item" href="#">Pie de Portada</a>
+				  </div>
+				</div>
+
+				<div class="dropdown opcion_menu">
+				  <button class="btn btn-link items dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				    Secciones Inicio
+				  </button>
+				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				    <a class="dropdown-item" href="#">Conoce mas...</a>
+				    <a class="dropdown-item" href="#">Noticias</a>
+				  </div>
+				</div>
+
+				<div class="dropdown opcion_menu">
+				  <button class="btn btn-link items dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				    Nosotros
+				  </button>
+				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				    <a class="dropdown-item" href="#">Imagen de Portada</a>
+				    <a class="dropdown-item" href="#">Contenido</a>
+				  </div>
+				</div>
+
+				</div>
+			</div>
+		</section>
+		<section class="row contenido">
+			<div class="col-md-2">
+				<button class="btn btn-secondary form-control mb-5 botones"><img src="icono_config/icono_lista.png">
+					&nbsp Lista de Usuario
+				</button>
+
+				<button class="btn btn-secondary form-control botones">
+				<img src="icono_config/icono_anadir.png">
+					&nbsp Agregar Usuario
+				</button>
+			</div>
+
+			<div class="col-md-8 offset-md-1">
+				<div class="card">
+				  <div class="card-header">
+				    Nuevo Usuario
+				  </div>
+				  <div class="card-body">
+
+				  	<div class="form-group row">
+				  		
+				  		<div class="col-sm-2"></div>
+				  		<div class="col-sm-4">
+				  	<input type="radio" name="usuario" value="Usuario administrador"> Usuario Administrador
+				  	</div>
+
+				       	<div class="col-sm-4">
+					<input type="radio" name="usuario" value="Usuario de Servicio"> Usuario de Servicio
+				    </div>
+					</div>
+
+				    <form>
+					  	<div class="form-group row">
+					    	<label for="descripcion" class="col-sm-3 col-form-label">Nombres:</label>
+					    	<div class="col-sm-8">
+					      		<input type="text" class="form-control" id="descripcion" placeholder="Ingrese sus Nombres">
+					    	</div>
+					  	</div>
+					  	<div class="form-group row">
+					    	<label for="enlace" class="col-sm-3 col-form-label">Apellidos:</label>
+					    	<div class="col-sm-8">
+					      		<input type="text" class="form-control" id="enlace" placeholder="Ingrese sus Apellidos">
+					    	</div>
+					  	</div>
+					  	<div class="form-group row">
+					    	<label for="posicion" class="col-sm-3 col-form-label">E-mail:</label>
+					    	<div class="col-sm-4">
+					      		<input type="text" class="form-control" id="posicion" placeholder="Ingrese su E-mail">
+					    	</div>
+					    </div>
+					    <div class="form-group row">
+					    	<label for="posicion" class="col-sm-3 col-form-label">Contraseña:</label>
+					    	<div class="col-sm-4">
+					      		<input type="password" class="form-control" id="posicion" placeholder="Ingrese su Contraseña">
+					    	</div>
+					  	</div>
+					  	
+
+					  	<div class="form-group row">
+					  		<div class="col-sm-3"></div>
+					    	<div class="col-sm-3">
+					    		<button class="btn btn-green text-center" style="background: green; border: green; color:white"><img src="icono_config/icono_actualizar.png">&nbsp Limpiar</button>
+					  	</div>
+
+					  	<div class="form-group row">
+					  		<div class="col-sm-3"></div>
+					      		<button class="btn btn-green text-center" style="background: green; border: green; color:white"><img src="icono_config/icono_guardar.png">&nbsp Guardar</button>
+					    	</div>
+					  	</div>
+						
+					</form>
+				  </div>
+				</div>
+			</div>
+		</section>
+
+	</div>
+
+	<script src="js/jquery.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ Ventana_emergente.html
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Agregar usuario</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<style>
+		header{
+			font-family: Notable;
+			color: #fff;
+			background-color: #3e3f42;
+			padding-top: 20px;
+			padding-left: 20px;
+			padding-bottom: 20px;
+			border-bottom: 4px #fff solid;
+		}
+
+		header .logo{
+			min-height: 62px;
+			max-height: 62px;
+			min-width: 155px;
+		}
+
+		.menu{
+			background-color: #f2f2f2;
+		}
+
+		.opcion_menu{
+			margin-right: 20px;
+		}
+
+		.items{
+			color: gray;
+			font-family: 'Georgia';
+		}
+
+		.contenido {
+			margin-top: 30px;
+		}
+	</style>
+</head>
+<body>
+<div class="container-fluid">
+		<header class="row">
+			<div class="col-3 col-xs-12">
+				<img  src="icono_config/logo.png" alt="Logo Empresa" class="img-fluid logo">
+			</div>
+			<div class="col-1 offset-4">
+				<img  src="icono_config/user.jpg" alt="Foto" class="rounded-circle foto" width="65" height="65">
+			</div>
+			<div class="col-4">
+				<p>Usuario Administrador</p>
+				<p>Magallanes Magallanes, Fáride</p>
+			<div class="col-1 offset-7">
+	 	<button class="btn btn-link" style= "position: absolute;bottom: 10px">
+		<img src="icono_config/icono_salir.png"></button>
+			</div>
+		</header>
+		<section class="row menu">
+			<div class="col-12">
+				<div class="btn-group" role="group" aria-label="Basic example">
+				
+				<div class="dropdown opcion_menu">
+				  <button class="btn btn-link items dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				    Acceso
+				  </button>
+				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				    <a class="dropdown-item" href="Cabecera.html">Menu 1</a>
+				    <a class="dropdown-item" href="#">Menu 2</a>
+				    <a class="dropdown-item" href="#">Menu 3</a>
+				  </div>
+				</div>
+		<div class="dropdown opcion_menu">
+				  <button class="btn btn-link items dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				    Portada Principal
+				  </button>
+				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				    <a class="dropdown-item" href="#">Cabecera de Portada</a>
+				    <a class="dropdown-item" href="#">Redes Sociales</a>
+				    <a class="dropdown-item" href="#">Slider</a>
+				    <a class="dropdown-item" href="#">Pie de Portada</a>
+				  </div>
+				</div>
+
+				<div class="dropdown opcion_menu">
+				  <button class="btn btn-link items dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				    Secciones Inicio
+				  </button>
+				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				    <a class="dropdown-item" href="#">Conoce mas...</a>
+				    <a class="dropdown-item" href="#">Noticias</a>
+				  </div>
+				</div>
+
+				<div class="dropdown opcion_menu">
+				  <button class="btn btn-link items dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				    Nosotros
+				  </button>
+				  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				    <a class="dropdown-item" href="#">Imagen de Portada</a>
+				    <a class="dropdown-item" href="#">Contenido</a>
+				  </div>
+				</div>
+
+				</div>
+			</div>
+
+			<div class="col-md-8 offset-md-1">
+				<div class="card">
+				  <div class="card-header">
+				    Usuario de Servicio
+				  </div>
+				  <div class="card-body">
+
+		<form method="post" action="registrar.php">
+
+<fieldset border="2px" style="width: 380px"> 
+	
+	<input type="checkbox" name="opcion" value="Administrador de Slider"> Administrador de Slider <br>
+	<input type="checkbox" name="opcion" value="Administrador de Nosotros"> Administrador de Nosotros <br>
+	<input type="checkbox" name="opcion" value="Administrador de Fondo"> Administrador de Fondo <br> <br>
+
+	<button class="btn btn-green text-center" style="background: green; border: green; color:white">
+	<img src="icono_config/icono_guardar.png">&nbsp Guardar</button>
+	<button class="btn btn-green text-center" style="background: green; border: green; color:white">
+	<img src="icono_config/icono_cerrar.png">&nbsp Cancelar</button>
+
+	</form>
+</fieldset>
+
+	<script src="js/jquery.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
