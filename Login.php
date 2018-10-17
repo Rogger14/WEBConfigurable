7905060f3328cd63e7dee3526e6c25565e69895e
EOrmenoY <eduardormeno7@gmail.com>
2018-10-17 17:30:32 -0500

    Login

---
+++ Login.php
@@ -0,0 +1,20 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Formulario de Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/estilo_login.css">
+	
+</head>
+	<body style ="background: linear-gradient(#CCF76F,#66CB17)">
+	
+	<form action="">
+		<h2>Iniciar Sesión</h2>
+		<input type="text" placeholder="&#128113; Usuario" name="usuario">
+		<input type="password" placeholder="&#128272; Contraseña" name="contraseña">
+		<input type="submit" value="Ingresar">
+		
+		</form>
+	</body>
+</html>
